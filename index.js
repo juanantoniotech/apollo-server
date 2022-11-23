659da3be4f833ba55504b79b3702895cd3ae9969
juanantonio <jcahuanam@gmail.com>
2022-11-23 18:27:57 -0500

    add schema and resolvers

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+// Schema
+const typeDefs = gql`
+  type Estudiante {
+    nombre: String
+    apellido: String
+  }
+
+  type Query {
+    obtenerEstudiantes: Estudiante
+  }
+`;
+
+const estudiantes = [
+  {
+    nombre: "Juan",
+    apellido: "Perez",
+  },
+  {
+    nombre: "Juan",
+    apellido: "Cahuana",
+  },
+];
+
+// Resolvers
+const resolvers = {
+  Query: {
+    obtenerEstudiantes: () => estudiantes[0],
+  },
+};
+
 // server
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 // run server
 server.listen().then(() => {
